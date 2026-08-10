9f54026c0ee2972f3393e82a0d0f4bfae47e461e
hichambaallal-prog <hichambaallal@gmail.com>
2026-08-10 13:05:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+st.set_page_config(page_title="LPEE - CTR-CSB", page_icon="🏗️", layout="wide", initial_sidebar_state="expanded")
 # Configurer la page (Optionnel)
 st.set_page_config(page_title="LPEE - CTR-CSB", page_icon="🏗️", layout="wide")
 
