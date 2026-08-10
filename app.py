5d305e0ea97c1466d0c3c956673d054562383d0f
hichambaallal-prog <hichambaallal@gmail.com>
2026-08-10 12:29:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,4 @@
 import streamlit as st
-
-# Définition des pages
-page_accueil = st.Page("views/accueil.py", title="Accueil", icon="🏠")
-page_plaque = st.Page("views/essai_plaque.py", title="Essai à la Plaque", icon="🪨")
-page_beton = st.Page("views/suivi_betonnage.py", title="Suivi de Bétonnage", icon="🏗️")
-
-# En-tête de la barre latérale
-st.sidebar.title("🏢 LPEE - CTR-CSB")
-st.sidebar.caption("Projet : LGV CASA SUD | Client : TGCC")
-
-# Navigation unifiée
-pg = st.navigation([page_accueil, page_plaque, page_beton])
-pg.run()
-import streamlit as st
 import pandas as pd
 from datetime import date
 from supabase import create_client, Client
